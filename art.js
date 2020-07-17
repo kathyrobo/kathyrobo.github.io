e3b7914a05f6c0cd109df7515fff40bcf4bad41c
sstevenshang <sstevenshang@gmail.com>
2020-07-16 23:13:47 -0700

    Add Kathy's website

---
+++ art.js
@@ -0,0 +1,26 @@
+function setup() {
+  
+  createCanvas(700,700)
+  frameRate(2)
+  background(204, 204, 204)
+
+  ellipse(250, 250, 250)
+  fill(250,250,250)
+  
+
+
+
+}
+
+function draw(){
+ strokeWeight(2)
+  
+  
+line(random(00),random(700),random(700),random(700)) }
+
+
+  function pink(){
+line(random(00),random(700),random(700),random(700)) 
+  fill(250, 237, 209)
+}
+
